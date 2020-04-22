0405bf2e4bbbb6b336f15048dd1a6235b5736d1e
Fabien LEGE <fabien.lege@ho5group.com>
2020-04-22 15:43:11 +0200

    ADD : add a hookable class

---
+++ src/Hookable.php
@@ -0,0 +1,23 @@
+<?php
+/***
+ *                                       _           _____
+ *                                      | |         |  ___|
+ *      __ _  __ _  ___ _ __   ___ ___  | |__   ___ |___ \
+ *     / _` |/ _` |/ _ \ '_ \ / __/ _ \ | '_ \ / _ \    \ \
+ *    | (_| | (_| |  __/ | | | (_|  __/ | | | | (_) /\__/ /
+ *     \__,_|\__, |\___|_| |_|\___\___| |_| |_|\___/\____/
+ *            __/ |
+ *           |___/
+ *
+ *           >> https://agenceho5.com
+ */
+
+namespace Nsi\Helpers;
+
+class Hookable extends Singleton {
+  public function __construct() {
+    static::hooks();
+  }
+
+  public static function hooks() {}
+}
